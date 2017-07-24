1e3c71c54d64b8b8d570616a7f53170e61aa0ff5
Claudius Mbemba <mbembac@gmail.com>
2017-07-24 05:03:41 -0700

    refactored and added model code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,13 +3,12 @@ module.exports = function(app) {
 
   var api = require('../controllers/apiController');
 
-
   app.route('/')
-    .get(api.home)
+    .get(api.home);
     
   app.route('/sketch')
-    .post(api.generate_sketch)
+    .post(api.generate_sketch);
 
   app.route('/model')
-    .post(api.generate_image_from_model)
+    .post(api.generate_image_from_model);
 };
