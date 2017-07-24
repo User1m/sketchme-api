be575ded47c33ff88b962b20a7ef87515ab79879
Claudius Mbemba <mbembac@gmail.com>
2017-07-24 00:28:01 -0700

    running python

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -26,8 +26,7 @@ function saveImageToDisk(data) {
 
 function executeSketchScript(){
 	console.log("RUNNING SKETCH SCRIPT.....");
-	// ${PIX_PATH}/dataset/PencilSketch/gen_sketch_and_gen_resized_face.py
-	PythonShell.run(`${PIX_PATH}/dataset/sketch.sh --image-path ${API_PATH}/uploads/${id}/image --face-path ${API_PATH}/uploads/${id}/face --sketch-path ${API_PATH}/uploads/${id}/sketch`, function (err) {
+	PythonShell.run(`${PIX_PATH}/dataset/PencilSketch/gen_sketch_and_gen_resized_face.py ${API_PATH}/uploads/${id}/image ${API_PATH}/uploads/${id}/face ${API_PATH}/uploads/${id}/sketch`, function (err) {
 		if (err) throw err;
 		console.log('finished');
 	});
