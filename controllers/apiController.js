a03480abba613501c1011335da566ef20d174b65
Claudius Mbemba <mbembac@gmail.com>
2017-07-23 23:34:04 -0700

    updates

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -6,69 +6,67 @@ const fs = require('fs');
 const WORKSPACE_PATH = "/home/user1m/workspace";
 const API_PATH = "/home/user1m/workspace/api";
 const PIX_PATH = "/home/user1m/workspace/sketch2pix";
+const id = uuidv4();
 
+function saveImageToDisk(data) {
+	//	var base64Data,
+	var binaryData;
 
-function getUUID(){
-	return uuidv4();
-}
-
-const id = getUUID();
-
-
-function saveImageToDisk(data){
-//	var base64Data,
-	var	binaryData;
-
-//	base64Data  =   data.replace(/^data:image\/png;base64,/, "");
-//	base64Data  =   data.replace(/^data:([A-Za-z-+\/]+);base64,(.+)$/, "");
-//	base64Data  +=  base64Data.replace('+', ' ');
-//	binaryData  =   new Buffer(base64Data, 'base64').toString('binary');
+	//	base64Data  =   data.replace(/^data:image\/png;base64,/, "");
+	//	base64Data  =   data.replace(/^data:([A-Za-z-+\/]+);base64,(.+)$/, "");
+	//	base64Data  +=  base64Data.replace('+', ' ');
+	//	binaryData  =   new Buffer(base64Data, 'base64').toString('binary');
 
 	shell.cd(`${API_PATH}/uploads/${id}`)
 	fs.writeFile(`${id}.jpg`, data, "binary", function (err) {
-		console.log(err); 
+		if (err) {
+			console.log(err);
+		} else {
+			console.log("IMAGE SAVED");
+		}
 	});
 	shell.cd(WORKSPACE_PATH)
 }
 
-exports.generate_sketch = function(req, res, next) {
+exports.generate_sketch = function (req, res, next) {
 	shell.mkdir("-p", `${API_PATH}/uploads/${id}`)
 	if (req.method == 'POST') {
-        var body = "";
+		var body = "";
 
-        req.on('data', function (data) {
-            body += data;
+		req.on('data', function (data) {
+			body += data;
 
-            // Too much POST data, kill the connection!
-            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-            if (body.length > (10 * Math.pow(10, 6)))
-                req.connection.destroy();
-        });
+			// 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+			if (body.length > (10 * Math.pow(10, 6))) {
+				res.send(`Image too large! Please upload an image under 10MB`);
+				req.connection.destroy();
+			}
+		});
 
-        req.on('end', function () {
+		req.on('end', function () {
 			saveImageToDisk(body);
 			res.send(`Done`);
-        });
-    }
-//	res.send("Hello Sketch");
-//	function puts(error, stdout, stderr) { sys.puts(stdout) }
-//	exec("ls -la", puts);
-	
-//  Task.find({}, function(err, task) {
-//    if (err)
-//      res.send(err);
-//    res.json(task);
-//  });
+		});
+	}
+	//	res.send("Hello Sketch");
+	//	function puts(error, stdout, stderr) { sys.puts(stdout) }
+	//	exec("ls -la", puts);
+
+	//  Task.find({}, function(err, task) {
+	//    if (err)
+	//      res.send(err);
+	//    res.json(task);
+	//  });
 };
 
 
-exports.generate_image_from_model = function(req, res, next) {
-  
-  	res.send("Hello Model");
+exports.generate_image_from_model = function (req, res, next) {
+
+	res.send("Hello Model");
 };
 
 
-exports.home = function(req, res, next){
+exports.home = function (req, res, next) {
 	var msg = `<h1>Welcome</h1><br/><p>Apis available:</p><ul><li>post: /sketch</li><li>post: /model</li></ul>`;
 	res.send(msg);
 };
