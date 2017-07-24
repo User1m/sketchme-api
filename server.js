71cacab8dd51257f803c54395a392d474190f39d
Claudius Mbemba <mbembac@gmail.com>
2017-07-24 11:49:27 -0700

    code cleanup

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
-//var express = require('express'),
-//  app = express(),
-//  port = process.env.PORT || 3000;
-
-//app.listen(port);
-
-//console.log('todo list RESTful API server started on: ' + port);
+'use strict';
 
 var express = require('express'),
   app = express(),
@@ -14,22 +8,16 @@ var express = require('express'),
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//app.use(function(req, res) {
-//  res.status(404).send({url: req.originalUrl + ' not found'})
-//});
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-// app.use(bodyParser({uploadDir:'./uploads'}));
 
 var routes = require('./routes/apiRoutes');
 routes(app);
 
-
 app.listen(port);
 
-console.log('RESTful API server started on: ' + port);
+console.log('API server started on: ' + port);
