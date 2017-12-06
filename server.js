4e4540fa12b992388aa327a31c1f4027d35f11ae
Claudius Mbemba <mbembac@gmail.com>
2017-12-05 23:35:05 -0800

    cors

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
 'use strict';
 
-var express = require('express'),
+const express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  cors = require('cors'),
+  corsOptions = {
+    origin: 'https://clmbsketchme.azurewebsites.net',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
 
-var cors = require('cors')
-
+app.options('*', cors(corsOptions));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
   next();
 });
 
-
-var routes = require('./routes/apiRoutes');
+const routes = require('./routes/apiRoutes');
 routes(app);
 
 app.listen(port);
