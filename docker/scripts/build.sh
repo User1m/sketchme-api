73cafb9749c78005074a033bcc32ec423f412a31
Claudius Mbemba <mbembac@gmail.com>
2018-04-01 19:01:47 -0700

    linking dockerignore before build

--- docker/scripts/build.sh
+++ docker/scripts/build.sh
@@ -5,6 +5,9 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
+ln -sf /Users/claudius/Documents/workspace/_ML/sketchme/sketchme-docker/sketchme-backend/sketchme-api/docker/.dockerignore \
+	/Users/claudius/Documents/workspace/_ML/sketchme/sketchme-docker/sketchme-backend/.dockerignore
+
 docker build -t sketchme-backend:prod-$1 \
 	-f /Users/claudius/Documents/workspace/_ML/sketchme/sketchme-docker/sketchme-backend/sketchme-api/docker/Dockerfile \
 	/Users/claudius/Documents/workspace/_ML/sketchme/sketchme-docker/sketchme-backend/.
